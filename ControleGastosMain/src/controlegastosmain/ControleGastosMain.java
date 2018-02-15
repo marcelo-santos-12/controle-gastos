ad90b7169295b67c172022076f718f7c78a80a31
marmh <marmh@marcelo-santos>
2018-02-15 00:05:53 -0200

    first github

---
+++ ControleGastosMain/src/controlegastosmain/ControleGastosMain.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlegastosmain;
+
+/**
+ *
+ * @author marcelo santos
+ */
+public class ControleGastosMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo do marcelo");
+    }
+    
+}
